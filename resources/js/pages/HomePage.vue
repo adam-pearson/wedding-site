b468b03d93e6c12845e4260e149014371768b4e1
Adam Pearson <adam.pearson94@gmail.com>
2024-02-04 11:05:00 +0000

    Added sidebar to homepage

--- resources/js/pages/HomePage.vue
+++ resources/js/pages/HomePage.vue
@@ -1,7 +1,19 @@
 <template>
-    <MainSection />
+    <div class="flex" id="oliven-page">
+        <SidebarSection />
+        <MainSection />
+    </div>
 </template>
 
 <script setup>
 import MainSection from '../components/MainSection.vue';
+import SidebarSection from '../components/SidebarSection.vue';
 </script>
+
+<style lang="postcss" scoped>
+#oliven-page {
+    width: 100%;
+    overflow: hidden;
+    position: relative;
+}
+</style>

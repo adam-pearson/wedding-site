a4ec37e25b048c90b5da22c3cece3e0446aa2e64
Adam Pearson <adam.pearson94@gmail.com>
2024-02-06 17:34:52 +0000

    Added proper widths and media queries to main section

--- resources/js/components/MainSection.vue
+++ resources/js/components/MainSection.vue
@@ -1,31 +1,16 @@
 <template>
-    <div class="flex flex-col" id="oliven-main">
+    <!-- eslint-disable max-len -->
+    <div
+        class="transition-width flex w-full flex-col duration-[0.5s] sm:w-[calc(100%-270px)] md:w-[70%] xl:w-[78%]"
+        id="oliven-main"
+    >
         <HeroSection />
         <FamilySection />
     </div>
+    <!-- eslint-enable max-len -->
 </template>
 
 <script setup>
 import HeroSection from './HeroSection.vue';
 import FamilySection from './FamilySection.vue';
 </script>
-
-<style lang="postcss" scoped>
-#oliven-main {
-    width: 78%;
-    float: right;
-    -webkit-transition: 0.5s;
-    -o-transition: 0.5s;
-    transition: 0.5s;
-}
-@media screen and (max-width: 1200px) {
-    #oliven-main {
-        width: 70%;
-    }
-}
-@media screen and (max-width: 768px) {
-    #oliven-main {
-        width: 100%;
-    }
-}
-</style>

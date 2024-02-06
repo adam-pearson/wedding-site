a7461b8bba8f6388477b7c7595e9b9a7693f8072
Adam Pearson <adam.pearson94@gmail.com>
2024-02-06 17:35:27 +0000

    Formatting

--- resources/js/components/SidebarSection.vue
+++ resources/js/components/SidebarSection.vue
@@ -1,40 +1,56 @@
 <template>
+    <!-- eslint-disable max-len -->
     <aside
-        class="flex flex-col justify-between py-16 px-8 fixed bottom-0 top-0 left-0 overflow-y-scroll z-[1001] bg-secondary duration-[0.5s] xl:w-[22%] md:w-[30%] w-[270px] sm:translate-x-[0] translate-x-[-270px]">
-
+        class="fixed bottom-0 left-0 top-0 z-[1001] flex w-[270px] translate-x-[-270px] flex-col justify-between overflow-y-scroll bg-secondary px-8 py-8 duration-[0.5s] sm:translate-x-[0] md:w-[30%] md:py-16 xl:w-[22%]"
+    >
         <!-- Logo -->
-        <div class="text-center pb-16 w-full text-black leading-5">
+        <div class="w-full pb-16 text-center leading-5 text-black">
             <a :href="route('home')" class="flex flex-col items-center">
-                <img class="w-[90px] mb-0" src="../../../resources/assets/logo.png" alt="">
-                <span class="font-script font-normal normal-case text-4xl text-primary">Heather <small>&</small> Adam</span>
-                <h6 class="font-sans mb-2 text-xs tracking-[5px]">02.10.2024</h6>
+                <img
+                    class="mb-0 w-[90px]"
+                    src="../../../resources/assets/logo.png"
+                    alt=""
+                />
+                <span
+                    class="font-script text-4xl font-normal normal-case text-primary"
+                    >Heather <small>&</small> Adam</span
+                >
+                <h6 class="mb-2 font-sans text-xs tracking-[5px]">
+                    02.10.2024
+                </h6>
             </a>
         </div>
         <!-- Menu -->
         <nav>
-            <ul class="text-center p-0 mx-0 mt-0 sm:mb-0 mb-8">
-                <li 
-                v-for="navItem in navItems" 
-                :key="navItem.text" 
-                class="my-[3px] pb-[3px]">
-                    <a :href="navItem.route"
-                    class="font-serif text-lg leading-6 tracking-wide duration-[0.3s] border-b border-b-transparent hover:border-b-primary hover:duration-[0.5s]">
+            <ul class="mx-0 mb-8 mt-0 p-0 text-center sm:mb-0">
+                <li
+                    v-for="navItem in navItems"
+                    :key="navItem.text"
+                    class="my-[3px] pb-[3px]"
+                >
+                    <a
+                        :href="navItem.route"
+                        class="border-b border-b-transparent font-serif text-lg leading-6 tracking-wide duration-[0.3s] hover:border-b-primary hover:duration-[0.5s]"
+                    >
                         {{ navItem.text }}
                     </a>
                 </li>
             </ul>
         </nav>
         <!-- Sidebar Footer -->
-        <div class="flex flex-col items-center pt-16 text-center m-0 text-xs tracking-wide text-gray-600 ">
-            <span class="w-20 border-b border-b-primary inline-block my-2"></span>
+        <div
+            class="m-0 flex flex-col items-center pt-16 text-center text-xs tracking-wide text-gray-600"
+        >
+            <span
+                class="my-2 inline-block w-20 border-b border-b-primary"
+            ></span>
             <p>Heather & Adam wedding<br />2nd October 2024</p>
         </div>
     </aside>
+    <!-- eslint-enable max-len -->
 </template>
 <script setup>
-
-const navItems = 
-[
+const navItems = [
     {
         text: 'Home',
         route: route('home'),
@@ -75,7 +91,7 @@ const navItems =
         text: 'Blog',
         route: route('home'),
     },
-]
+];
 </script>
 
 <style lang="postcss" scoped>
@@ -88,25 +104,11 @@ const navItems =
 }
 
 ::-webkit-scrollbar-thumb {
-    background: #BD945A;
+    background: #bd945a;
 }
 
 ::-webkit-scrollbar-thumb:hover {
-    background: #BD945A;
-}
-
-@media screen and (max-width: 768px) {
-    #oliven-aside {
-        width: 270px;
-        transform: translateX(-270px);
-        padding-top: 30px;
-    }
-}
-
-@media screen and (max-width: 768px) {
-    #oliven-aside .oliven-logo {
-        margin-bottom: 60px;
-    }
+    background: #bd945a;
 }
 
 #oliven-aside .footer1 {
@@ -137,4 +139,5 @@ const navItems =
         position: relative;
         padding-top: 60px;
     }
-}</style>
+}
+</style>

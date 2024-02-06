93d25df2981cf3d26725229b9e6299d4fbe3684c
Adam Pearson <adam.pearson94@gmail.com>
2024-02-06 23:25:29 +0000

    Finished countdown section, responsiveness etc

--- resources/js/components/FamilySection.vue
+++ resources/js/components/FamilySection.vue
@@ -1,26 +1,53 @@
 <template>
-    <div id="countdown" class="py-[100px] text-center bg-red-500">
+    <!-- eslint-disable max-len -->
+    <div
+        id="countdown"
+        class="min-h-96 bg-[url(/resources/assets/countdownBackground.jpg)] bg-cover bg-fixed bg-center bg-no-repeat py-[100px] text-center"
+    >
         <div>
             <div>
                 <div class="mb-4">
-                    <h4 class="text-white font-script text-6xl">We will become a family<span v-if="!weddingHasStarted"> in</span></h4> 
+                    <h4 class="font-script text-3xl text-white md:text-6xl">
+                        We will become a family<span v-if="!weddingHasStarted">
+                            in</span
+                        >
+                    </h4>
                 </div>
             </div>
             <div>
                 <div v-if="!weddingHasStarted" class="countdown-time">
                     <ul>
-                        <li><span id="days">{{ timeRemaining?.days }}</span>Days</li>
-                        <li><span id="hours">{{ timeRemaining?.hours }}</span>Hours</li>
-                        <li><span id="minutes">{{ timeRemaining?.minutes }}</span>Minutes</li>
-                        <li><span id="seconds">{{ timeRemaining?.seconds }}</span>Seconds</li>
+                        <li>
+                            <span id="days">{{ timeRemaining?.days }}</span
+                            >Days
+                        </li>
+                        <li>
+                            <span id="hours">{{ timeRemaining?.hours }}</span
+                            >Hours
+                        </li>
+                        <li>
+                            <span id="minutes">{{
+                                timeRemaining?.minutes
+                            }}</span
+                            >Minutes
+                        </li>
+                        <li>
+                            <span id="seconds">{{
+                                timeRemaining?.seconds
+                            }}</span
+                            >Seconds
+                        </li>
                     </ul>
                 </div>
                 <div v-else class="countdown-time">
-                    <h4 class="text-white font-script text-6xl">Today!</h4>
+                    <h4 class="font-script text-3xl text-white md:text-6xl">
+                        Today!
+                    </h4>
                 </div>
             </div>
         </div>
     </div>
+    <!-- eslint-enable max-len -->
 </template>
 
 <script setup>
@@ -35,39 +62,36 @@ const timeRemaining = reactive({
     hours: 0,
     minutes: 0,
     seconds: 0,
-})
+});
 
 onBeforeMount(() => {
     const second = 1000;
     const minute = second * 60;
     const hour = minute * 60;
     const day = hour * 24;
-    
+
     const endTime = new Date(WEDDING_DATE).getTime();
 
     const timingInterval = setInterval(() => {
         const now = new Date().getTime();
         const distance = endTime - now;
-        
 
         if (distance < 0) {
             weddingHasStarted.value = true;
 
-            console.log('WEDDING TIME!');
             clearInterval(timingInterval);
         }
-        
-        timeRemaining.days = Math.floor(distance / day);
-        timeRemaining.hours = Math.floor((distance % (day)) / hour);
-        timeRemaining.minutes = Math.floor((distance % (hour)) / minute);
-        timeRemaining.seconds = Math.floor((distance % (minute)) / second);
-    }, 0)
-});
 
+        timeRemaining.days = Math.floor(distance / day);
+        timeRemaining.hours = Math.floor((distance % day) / hour);
+        timeRemaining.minutes = Math.floor((distance % hour) / minute);
+        timeRemaining.seconds = Math.floor((distance % minute) / second);
+    }, 0);
+});
 </script>
 
 <style scoped lang="postcss">
-.countdown-time span{
+.countdown-time span {
     @apply font-sans;
     @apply text-6xl;
     @apply block;
@@ -77,9 +101,12 @@ onBeforeMount(() => {
 .countdown-time li {
     @apply inline-block;
     @apply text-white;
-    @apply p-4;
+    @apply px-4;
+    @apply md:p-4;
     @apply leading-['3rem'];
     @apply uppercase;
     @apply tracking-wider;
+    @apply text-xs;
+    @apply md:text-base;
 }
 </style>

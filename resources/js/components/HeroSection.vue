445dcb09b6639451e043f94db9fe0ea98b888024
Adam Pearson <adam.pearson94@gmail.com>
2024-02-06 17:34:22 +0000

    Removed unnecessary classes

--- resources/js/components/HeroSection.vue
+++ resources/js/components/HeroSection.vue
@@ -1,20 +1,20 @@
 <template>
-    <header id="home" class="flex justify-center items-center justify-center h-screen hero-image">
+    <header id="home" class="flex justify-center items-center h-screen hero-image">
         <div>
             <div>
-                <div class="col-md-12 text-center caption">
-                    <h5>The wedding of</h5>
-                    <h1>Heather & Adam</h1>
-                    <h5>2nd October, 2024</h5>
+                <div class="text-center">
+                    <h5 class="hero-caption">The wedding of</h5>
+                    <h1 class="hero-names">Heather & Adam</h1>
+                    <h5 class="hero-caption">2nd October, 2024</h5>
                 </div>
             </div>
-            <div class="row">
-                <div class="col-md-12">
+            <!-- <div>
+                <div>
                     <div class="arrow bounce text-center">
                         <a href="index.html#couple"> <i class="ti-heart"></i> </a>
                     </div>
                 </div>
-            </div>
+            </div> -->
         </div>
     </header>
 </template>
@@ -25,59 +25,25 @@
 
 <style scoped lang="postcss">
 .hero-image {
-    background-image: url(/resources/assets/heroImage.jpg);
-    background-size: cover;
-    background-repeat: no-repeat;
-    background-position-x: 50%;
-    overflow:hidden;
+    @apply bg-[url(/resources/assets/heroImage.jpg)];
+    @apply bg-cover;
+    @apply bg-no-repeat;
+    @apply bg-center;
+    @apply overflow-hidden;
+}
+.hero-names {
+    @apply font-script;
+    @apply mb-4;
+    @apply text-[7.5rem];
+    @apply text-white;
+    @apply leading-[7.5rem];
 }
 
-header .caption h1 {
-    font-family: 'Alex Brush', cursive;
-    font-weight: 400;
-    margin: 0 0 15px 0;
-    font-size: 120px;
-    color: #FFF;
-    display: block;
-    line-height: 1em;
-    position: relative;
-}
-
-header .caption h5 {
+.hero-caption {
     @apply font-sans;
-     font-size: 20px;
-     font-weight: 400;
-     letter-spacing: 5px;
-     margin-bottom: 0;
-     color: #fff;
-     text-transform: uppercase;
+    @apply text-xl;
+    @apply tracking-[5px];
+    @apply text-white;
+    @apply uppercase;
  }
-
- .header .arrow {
-    position: relative;
-    top: 27vh;
-    width: 100%;
-    text-align: center;
-    z-index: 8;
-}
-.header .arrow.bounce {
-    -webkit-animation: bounce 2s infinite;
-    animation: bounce 2s infinite;
-}
-.header .arrow i {
-    position: relative;
-    display: inline-block;
-    width: 40px;
-    height: 40px;
-    line-height: 40px;
-    color: #fff;
-    font-size: 20px;
-}
-.header .arrow i:hover:after {
-    -webkit-transform: scale(1.2);
-    transform: scale(1.2);
-    opacity: 0;
-    -webkit-transition: all .5s;
-    transition: all .5s;
-}
 </style>

aeadfeea8b8439129372f888f07ee0ad74d67e78
Adam Pearson <adam.pearson94@gmail.com>
2024-03-03 20:21:15 +0000

    Added route for storing guests

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,7 @@
 <?php
 
-use Illuminate\Http\Request;
+use App\Http\Controllers\AddGuestController;
+use App\Http\Middleware\Authenticated;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -14,6 +15,7 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
+Route::middleware('auth:sanctum')->group(function () {
+    Route::post(uri: '/guests/store', action: AddGuestController::class)
+        ->name('admin.guests.store');
 });

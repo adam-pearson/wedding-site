4a2baee5f0fb589b7b1a3c20a80b81f295138ca9
Adam Pearson <adam.pearson94@gmail.com>
2024-03-07 23:26:00 +0000

    Fix migration casing

--- database/migrations/2024_03_03_231512_add_is_child_to_guests_table.php
+++ database/migrations/2024_03_03_231512_add_is_child_to_guests_table.php
@@ -11,7 +11,7 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::table('Guests', function (Blueprint $table) {
+        Schema::table('guests', function (Blueprint $table) {
             $table->boolean('is_child')->default(false);
         });
     }
@@ -21,7 +21,7 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::table('Guests', function (Blueprint $table) {
+        Schema::table('guests', function (Blueprint $table) {
             $table->dropColumn('is_child');
         });
     }

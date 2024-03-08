4195c6db4083079461bdd1f5177e1c4dc9a13f3a
Adam Pearson <adam.pearson94@gmail.com>
2024-03-08 00:01:47 +0000

    Fixed z-index of loading overlay

--- resources/js/components/Shared/LoadingOverlay.vue
+++ resources/js/components/Shared/LoadingOverlay.vue
@@ -1,7 +1,7 @@
 <template>
   <!-- eslint-disable max-len -->
   <div
-    class="w-full h-full backdrop-blur-sm absolute flex items-center justify-center transition duration-150"
+    class="w-full h-full backdrop-blur-sm absolute flex items-center justify-center transition duration-150 z-50"
   >
     <div
       class="inline-block h-8 w-8 animate-spin rounded-full text-amber-500 border-4 border-solid border-current border-e-transparent align-[-0.125em] text-surface motion-reduce:animate-[spin_1.5s_linear_infinite] dark:text-white"

9c6d29092ada2873add26a530cce61a483e5a154
Adam Pearson <adam.pearson94@gmail.com>
2024-10-05 10:22:58 +0100

    Fixed email conditions

--- resources/views/emails/rsvp/rsvp_response.blade.php
+++ resources/views/emails/rsvp/rsvp_response.blade.php
@@ -17,17 +17,18 @@
 <p>New RSVP response submitted by <strong>{{$guest->name}} (id: {{$guest->id}})</strong></p>
 
 <h2>Guest Details</h2>
+
 <ul>
     <li><strong>Name:</strong> {{$guest->name}}</li>
     <li><strong>Email:</strong> {{$guest->email ?? '----'}}</li>
     <li><strong>Phone:</strong> {{$guest->phone ?? '----'}}</li>
     <li><strong>Coming:</strong> {{$rsvp->coming ? 'Yes' : 'No'}}</li>
-    <li><strong>Guest Type:</strong> {{$guest->guestType === 'all_day' ? 'All Day' : 'Evening'}}</li>
+    <li><strong>Guest Type:</strong> {{$guest->guestType->value === 'all_day' ? 'All Day' : 'Evening'}}</li>
     @if($rsvp->coming)
         @if($guest->plusOneAllowed)
             <li><strong>Bringing Plus One:</strong> {{$rsvp->bringingPlusOne ? 'Yes' : 'No'}}</li>
         @endif
-        @if($guest->guestType === 'all_day')
+        @if($guest->guestType->value === 'all_day')
             <li><strong>Alcohol:</strong> {{$rsvp->alcohol ? 'Yes' : 'No'}}</li>
         @endif
         <li><strong>Dietary Requirements:</strong> {{$rsvp->dietaryRequirements ?? '----'}}</li>
@@ -41,7 +42,7 @@
     <h2>Plus One Details</h2>
     <ul>
         <li><strong>Name:</strong> {{$plusOne->name}}</li>
-        @if($plusOne->guestType === 'all_day')
+        @if($plusOne->guestType->value === 'all_day')
             <li><strong>Alcohol:</strong> {{$plusOneRsvp->alcohol ? 'Yes' : 'No'}}</li>
         @endif
         <li><strong>Dietary Requirements:</strong> {{$plusOneRsvp->dietaryRequirements ?? '----'}}</li>
